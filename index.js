4dbdfab5d610671209343bf397563bf9aa805506
leadpython <jeffrey.campecino@gmail.com>
2017-06-09 08:45:59 -0700

    mongodb running

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = new express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const routes = require('./routes/api');
 
+mongoose.connect('mongodb://localhost/mill');
+
 app.use(bodyParser.json());
+mongoose.Promise = global.Promise;
 app.use('/api', routes);
 
 app.listen( 3000, () => {
--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const Vendor = require('../models/vendor');
+
 router.get('/vendors', (request, response) => {
   response.send( { type: 'GET' });
-})
+});
 
 router.post('/vendors', (request, response) => {
-  console.log(request.body);
-  response.send( { type: 'POST', body: request.body });
-})
+  Vendor.create(request.body).then((vendor) => {
+    response.send(vendor);
+  });
+});
 
 router.put('/vendors/:id', (request, response) => {
   response.send( { type: 'PUT' });
-})
+});
 
 router.delete('/vendors/:id', (request, response) => {
   response.send( { type: 'POST' });
-})
+});
 
 module.exports = router;
