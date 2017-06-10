64ef58dfc663b70c5cb941e5839d2eb8863349c6
leadpython <jeffrey.campecino@gmail.com>
2017-06-09 19:53:46 -0700

    RESTful api implemented

--- routes/api.js
+++ routes/api.js
@@ -7,18 +7,24 @@ router.get('/vendors', (request, response) => {
   response.send( { type: 'GET' });
 });
 
-router.post('/vendors', (request, response) => {
+router.post('/vendors', (request, response, next) => {
   Vendor.create(request.body).then((vendor) => {
     response.send(vendor);
-  });
+  }).catch(next);
 });
 
 router.put('/vendors/:id', (request, response) => {
-  response.send( { type: 'PUT' });
+  Vendor.findByIdAndUpdate({_id: request.params.id}, request.body).then(() => {
+    Vendor.findOne({_id: request.params.id}).then((vendor) => {
+      response.send(vendor);
+    });
+  });
 });
 
 router.delete('/vendors/:id', (request, response) => {
-  response.send( { type: 'POST' });
+  Vendor.findByIdAndRemove({_id: request.params.id}).then((vendor) => {
+    response.send(vendor);
+  });
 });
 
 module.exports = router;
