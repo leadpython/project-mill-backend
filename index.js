8de70c1159da887b9efcd07bf3d04f1f097dbe96
leadpython <jeffrey.campecino@gmail.com>
2017-06-10 06:40:29 -0700

    mongodb

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes/api');
 
-mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/mill');
 
 app.use(bodyParser.json());
 mongoose.Promise = global.Promise;
