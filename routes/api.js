a87c1c54c41a53d1cf376392cf99519db749b750
leadpython <jeffrey.campecino@gmail.com>
2017-06-14 21:26:49 -0700

    l

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoUtility = require('./../mongoUtilities');
+var database = mongoUtility.getDatabase();
+const collectionName = 'vendors';
+
 // Routes
 var VendorsRoute = require('./vendors.route');
 
 // Vendors Routes
 router.get('/vendors', (request, response) => {
-  VendorsRoute.retrieveVendors(request, response);
+    database.collection(collectionName).find({}).toArray((error, data) => {
+      if (error) {
+        handleError(response, error.message, "Failed to get contacts.");
+      } else {
+        response.status(200).json(data);
+      }
+    });
 });
 
 router.post('/vendors', (request, response) => {
