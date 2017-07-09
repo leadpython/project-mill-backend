b15eed54e937d79098bce07915f0d4ee84b81831
leadpython <jeffrey.campecino@gmail.com>
2017-07-09 14:17:26 -0700

    removed then method

--- routes/vendors.route.js
+++ routes/vendors.route.js
@@ -3,9 +3,8 @@ var _database;
 
 class VendorRoute {
   getVendors(request, response) {
-    _database.collection(collectionName).find({}).then((data) => {
-      response.status(200).json(data);
-    });
+    let vendors = _database.collection(collectionName).find({});
+    response.status(200).json(vendors);
   }
   updateVendor(request, response) {
   }
